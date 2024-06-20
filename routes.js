a8ea5ddc203fe963eef41f8e200f0fd871f670d3
Adrian Jerez <69713328+adriiiian@users.noreply.github.com>
2024-06-20 23:14:04 +0800

    typo fix

--- routes.js
+++ routes.js
@@ -4,7 +4,7 @@ const GenerateCollection = require("./api/generateCollection");
 const Router = _Router();
 
 Router.get("/", async (req, res) => {
-  res.statys(200).send({ message: "artGen2V1" });
+  res.status(200).send({ message: "artGen2V1" });
 });
 
 Router.post("/", GenerateCollection);
