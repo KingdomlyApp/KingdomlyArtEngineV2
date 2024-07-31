f41c92ef8aa060f1f7eed75a20aa3dd2ea259790
Adrian Jerez <69713328+adriiiian@users.noreply.github.com>
2024-07-31 18:49:11 +0800

    Added cors handling

--- index.js
+++ index.js
@@ -2,11 +2,30 @@ require("dotenv").config();
 const express = require("express");
 const json = express.json;
 const path = require("path");
+const cors = require("cors");
 const Router = require("./routes");
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "POST, GET",
+  allowedHeaders: "*",
+};
 
 const app = express();
 
 app.use(json({ limit: "50mb" }));
+
+app.use("/", cors(corsOptions), (req, res, next) => {
+  next();
+});
 
 const PORT = process.env.PORT || 3001;
 app.use("/", Router);
